6703b2d22522132b2fdc9e0cb103fad0e653afa0
sekhriakrem381-wq <sekhriakrem381@gmail.com>
2025-10-16 17:30:58 +0100

    Update server.js

--- server.js
+++ server.js
@@ -67,4 +67,6 @@ app.post('/create-order', async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}.`));```
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
